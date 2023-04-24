5c651b9c07063cae7e57611eb725d9efdc2cf2fa
Mark <markdavies550@gmail.com>
2023-04-24 07:26:40 +0100

    changing the default lsp

--- lua/lvim/lsp/config.lua
+++ lua/lvim/lsp/config.lua
@@ -1,51 +1,51 @@
 local skipped_servers = {
-  "angularls",
-  "ansiblels",
-  "antlersls",
-  "ccls",
-  "csharp_ls",
-  "cssmodules_ls",
-  "denols",
-  "docker_compose_language_service",
-  "ember",
-  "emmet_ls",
-  "eslint",
-  "eslintls",
-  "glint",
-  "golangci_lint_ls",
-  "gradle_ls",
-  "graphql",
-  "jedi_language_server",
-  "ltex",
-  "neocmake",
-  "ocamlls",
-  "phpactor",
-  "psalm",
-  "pylsp",
-  "pyre",
-  "quick_lint_js",
-  "reason_ls",
-  "rnix",
-  "rome",
-  "ruby_ls",
-  "ruff_lsp",
-  "scry",
-  "solang",
-  "solc",
-  "solidity_ls",
-  "sorbet",
-  "sourcekit",
-  "sourcery",
-  "spectral",
-  "sqlls",
-  "sqls",
-  "stylelint_lsp",
-  "svlangserver",
-  "tflint",
-  "unocss",
-  "verible",
-  "vtsls",
-  "vuels",
+    "angularls",
+    "ansiblels",
+    "antlersls",
+    "ccls",
+    "omnisharp",
+    "cssmodules_ls",
+    "denols",
+    "docker_compose_language_service",
+    "ember",
+    "emmet_ls",
+    "eslint",
+    "eslintls",
+    "glint",
+    "golangci_lint_ls",
+    "gradle_ls",
+    "graphql",
+    "jedi_language_server",
+    "ltex",
+    "neocmake",
+    "ocamlls",
+    "phpactor",
+    "psalm",
+    "pylsp",
+    "pyre",
+    "quick_lint_js",
+    "reason_ls",
+    "rnix",
+    "rome",
+    "ruby_ls",
+    "ruff_lsp",
+    "scry",
+    "solang",
+    "solc",
+    "solidity_ls",
+    "sorbet",
+    "sourcekit",
+    "sourcery",
+    "spectral",
+    "sqlls",
+    "sqls",
+    "stylelint_lsp",
+    "svlangserver",
+    "tflint",
+    "unocss",
+    "verible",
+    "vtsls",
+    "vuels",
 }
 
 local skipped_filetypes = { "markdown", "rst", "plaintext", "toml", "proto" }
@@ -53,97 +53,97 @@ local skipped_filetypes = { "markdown", "rst", "plaintext", "toml", "proto" }
 local join_paths = require("lvim.utils").join_paths
 
 return {
-  templates_dir = join_paths(get_runtime_dir(), "site", "after", "ftplugin"),
-  diagnostics = {
-    signs = {
-      active = true,
-      values = {
-        { name = "DiagnosticSignError", text = lvim.icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = lvim.icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = lvim.icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = lvim.icons.diagnostics.Information },
-      },
+    templates_dir = join_paths(get_runtime_dir(), "site", "after", "ftplugin"),
+    diagnostics = {
+        signs = {
+            active = true,
+            values = {
+                { name = "DiagnosticSignError", text = lvim.icons.diagnostics.Error },
+                { name = "DiagnosticSignWarn",  text = lvim.icons.diagnostics.Warning },
+                { name = "DiagnosticSignHint",  text = lvim.icons.diagnostics.Hint },
+                { name = "DiagnosticSignInfo",  text = lvim.icons.diagnostics.Information },
+            },
+        },
+        virtual_text = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = true,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
     },
-    virtual_text = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
+    document_highlight = false,
+    code_lens_refresh = true,
     float = {
-      focusable = true,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
+        focusable = true,
+        style = "minimal",
+        border = "rounded",
     },
-  },
-  document_highlight = false,
-  code_lens_refresh = true,
-  float = {
-    focusable = true,
-    style = "minimal",
-    border = "rounded",
-  },
-  on_attach_callback = nil,
-  on_init_callback = nil,
-  automatic_configuration = {
-    ---@usage list of servers that the automatic installer will skip
-    skipped_servers = skipped_servers,
-    ---@usage list of filetypes that the automatic installer will skip
-    skipped_filetypes = skipped_filetypes,
-  },
-  buffer_mappings = {
-    normal_mode = {
-      ["K"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Show hover" },
-      ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Goto definition" },
-      ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Goto Declaration" },
-      ["gr"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "Goto references" },
-      ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Goto Implementation" },
-      ["gs"] = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "show signature help" },
-      ["gl"] = {
-        function()
-          local config = lvim.lsp.diagnostics.float
-          config.scope = "line"
-          vim.diagnostic.open_float(0, config)
-        end,
-        "Show line diagnostics",
-      },
+    on_attach_callback = nil,
+    on_init_callback = nil,
+    automatic_configuration = {
+        ---@usage list of servers that the automatic installer will skip
+        skipped_servers = skipped_servers,
+        ---@usage list of filetypes that the automatic installer will skip
+        skipped_filetypes = skipped_filetypes,
     },
-    insert_mode = {},
-    visual_mode = {},
-  },
-  buffer_options = {
-    --- enable completion triggered by <c-x><c-o>
-    omnifunc = "v:lua.vim.lsp.omnifunc",
-    --- use gq for formatting
-    formatexpr = "v:lua.vim.lsp.formatexpr(#{timeout_ms:500})",
-  },
-  ---@usage list of settings of nvim-lsp-installer
-  installer = {
-    setup = {
-      ensure_installed = {},
-      automatic_installation = {
-        exclude = {},
-      },
+    buffer_mappings = {
+        normal_mode = {
+            ["K"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Show hover" },
+            ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Goto definition" },
+            ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Goto Declaration" },
+            ["gr"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "Goto references" },
+            ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Goto Implementation" },
+            ["gs"] = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "show signature help" },
+            ["gl"] = {
+                function()
+                    local config = lvim.lsp.diagnostics.float
+                    config.scope = "line"
+                    vim.diagnostic.open_float(0, config)
+                end,
+                "Show line diagnostics",
+            },
+        },
+        insert_mode = {},
+        visual_mode = {},
     },
-  },
-  nlsp_settings = {
-    setup = {
-      config_home = join_paths(get_config_dir(), "lsp-settings"),
-      -- set to false to overwrite schemastore.nvim
-      append_default_schemas = true,
-      ignored_servers = {},
-      loader = "json",
+    buffer_options = {
+        --- enable completion triggered by <c-x><c-o>
+        omnifunc = "v:lua.vim.lsp.omnifunc",
+        --- use gq for formatting
+        formatexpr = "v:lua.vim.lsp.formatexpr(#{timeout_ms:500})",
     },
-  },
-  null_ls = {
-    setup = {
-      debug = false,
+    ---@usage list of settings of nvim-lsp-installer
+    installer = {
+        setup = {
+            ensure_installed = {},
+            automatic_installation = {
+                exclude = {},
+            },
+        },
     },
-    config = {},
-  },
-  ---@deprecated use lvim.lsp.automatic_configuration.skipped_servers instead
-  override = {},
-  ---@deprecated use lvim.lsp.installer.setup.automatic_installation instead
-  automatic_servers_installation = nil,
+    nlsp_settings = {
+        setup = {
+            config_home = join_paths(get_config_dir(), "lsp-settings"),
+            -- set to false to overwrite schemastore.nvim
+            append_default_schemas = true,
+            ignored_servers = {},
+            loader = "json",
+        },
+    },
+    null_ls = {
+        setup = {
+            debug = false,
+        },
+        config = {},
+    },
+    ---@deprecated use lvim.lsp.automatic_configuration.skipped_servers instead
+    override = {},
+    ---@deprecated use lvim.lsp.installer.setup.automatic_installation instead
+    automatic_servers_installation = nil,
 }
